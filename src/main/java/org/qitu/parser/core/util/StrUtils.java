2be8f95779a2de7422854372cdc81370d7df4bff
zoudingyun <zoudingyun@foxmail.com>
2024-07-05 11:52:59 +0800

    补全备注

--- src/main/java/org/qitu/parser/core/util/StrUtils.java
+++ src/main/java/org/qitu/parser/core/util/StrUtils.java
@@ -14,6 +14,8 @@ public class StrUtils {
 
     /**
      * 是否为空白字符串
+     * @param string 待判断字符串
+     * @return 是否为空字符串（true：空字符串 false：非空）
      * */
     public static boolean isBlank(String string) {
         return StrUtil.isBlankIfStr(string);
@@ -21,6 +23,8 @@ public class StrUtils {
 
     /**
      * 去除首尾空白字符
+     * @param string 待处理字符串
+     * @return 处理后的字符串
      * */
     public static String trim(String string) {
         return StrUtil.trim(string);
